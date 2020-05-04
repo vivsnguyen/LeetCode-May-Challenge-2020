8cc5a3bd14c4a3ba84abddbe0e9651394e283268
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-05-04 12:06:04 -0700

    Submission first_bad_version

--- first_bad_version.py
+++ first_bad_version.py
@@ -22,5 +22,42 @@ call isBadVersion(3) -> false
 call isBadVersion(5) -> true
 call isBadVersion(4) -> true
 
-Then 4 is the first bad version. 
+Then 4 is the first bad version.
 """
+
+# first Solution; Time Limit Exceeded
+
+# class Solution:
+#     def firstBadVersion(self, n):
+#         """
+#         :type n: int
+#         :rtype: int
+#         """
+#         i = 0
+#         while not isBadVersion(i):
+#             i += 1
+            
+#         return i
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left = 1
+        right = n
+        
+        while left < right:
+            mid = left + (right - left) // 2;
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
