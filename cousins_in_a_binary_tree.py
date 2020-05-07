ff0f3072fe325bdeebb5641010696e41dd692e19
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-05-07 11:54:44 -0700

    submission cousins_in_a_binary_tree.py

--- cousins_in_a_binary_tree.py
+++ cousins_in_a_binary_tree.py
@@ -51,3 +51,41 @@ Note:
 The number of nodes in the tree will be between 2 and 100.
 Each node has a unique integer value from 1 to 100.
 """
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        
+        def find_height(root, parent, value, height):
+            if not root:
+                return 0
+            elif root.val == value:
+                return (height, parent)
+            
+            parent = root.val
+            left = find_height(root.left, parent, value, height+1)
+            if left:
+                return left
+            
+            parent = root.val
+            right = find_height(root.right, parent, value, height+1)
+            if right:
+                return right
+        
+        if root.val == x or root.val == y: 
+            return False
+        
+        x_height, x_parent = find_height(root, -1, x, 0)
+        
+        y_height, y_parent = find_height(root, -1, y, 0)
+        
+        
+        if x_parent != y_parent and x_height == y_height:
+            return True
+        
+        return False
