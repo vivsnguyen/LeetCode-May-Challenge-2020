80d9a21e94f7c6d462f9bf6e492d22c62377ae2d
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-05-11 11:09:39 -0700

    submission valid_perfect_square

--- valid_perfect_square.py
+++ valid_perfect_square.py
@@ -13,3 +13,12 @@ Example 2:
 Input: 14
 Output: false
 """
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        i = 1
+        
+        while i*i < num:
+            i += 1
+            
+        return i*i == num
