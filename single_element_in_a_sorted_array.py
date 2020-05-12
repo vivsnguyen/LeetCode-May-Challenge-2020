899fa1bf245d3d62cec4ee970a27331a8602f23a
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-05-12 15:38:52 -0700

    submission BST single element in a sorted array

--- single_element_in_a_sorted_array.py
+++ single_element_in_a_sorted_array.py
@@ -18,3 +18,31 @@ Output: 10
 
 Note: Your solution should run in O(log n) time and O(1) space.
 """
+#binary search
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        low = 0
+        high = len(nums)-1    
+        
+        if high == 0:
+            return nums[0]
+        elif nums[0] != nums[1]:
+            return nums[0]
+        elif nums[high] != nums[high-1]:
+            return nums[high]
+        
+        while low <= high:
+            mid = low + (high-low) // 2
+            
+            if nums[mid] != nums[mid-1] and nums[mid] != nums[mid+1]:
+                return nums[mid]
+            
+            else:
+                if (mid%2 == 0 and nums[mid] == nums[mid+1]) or (mid%2 == 1 and nums[mid] == nums[mid-1]):
+                    low = mid + 1
+                    
+                else:
+                    high = mid - 1
+                    
+        return -1
