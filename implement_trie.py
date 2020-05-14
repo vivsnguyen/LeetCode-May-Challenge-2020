d95510664ff34aa7d534d67d6f5dd79040409e93
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-05-14 15:10:03 -0700

    submission implement_trie

--- implement_trie.py
+++ implement_trie.py
@@ -20,3 +20,63 @@ lowercase letters a-z.
 
 All inputs are guaranteed to be non-empty strings.
 """
+#nested dicts
+
+class Trie(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.trie = {}
+        
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: None
+        """
+        t = self.trie
+        for char in word:
+            if char not in t: 
+                t[char] = {}
+            t = t[char]
+        t["is_Word"] = True
+        
+        
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        t = self.trie
+        for char in word:
+            if char not in t: 
+                return False
+            t = t[char]
+        return "is_Word" in t
+        
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        t = self.trie
+        for char in prefix:
+            if char not in t: 
+                return False
+            t = t[char]
+        return True
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
