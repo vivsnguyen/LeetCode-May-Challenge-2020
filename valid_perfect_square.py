d834d5dcc1c95a5af095569ef94109d19db32f8e
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-05-20 22:27:10 -0700

    added additional solution

--- valid_perfect_square.py
+++ valid_perfect_square.py
@@ -24,6 +24,17 @@ class Solution:
             
         return i*i == num
 
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        i = 1
+        
+        while i*i <= num:
+            if i*i == num:
+                return True
+
+            i += 1
+            
+        return False
 
 #binary search
 class Solution:
